766d55a40aadacf450495f0ec02f2bb826688199
Nikita Grachev <nik@sibvaleo.com>
2014-06-03 13:06:49 +0700

    Added. print mode

--- revision/saldo_gap.js
+++ revision/saldo_gap.js
@@ -17,9 +17,9 @@ var sw_grid2 = new dhtmlXGridObject({
     parent: "sw_grid2",
     columns: [
         { type:"ro", sort:"str",  align:"center", width:"80",  label:"Адрес" },
-        { type:"ro", sort:"str",  align:"left"  , width:"100", label:"Партия" },
-        { type:"ro", sort:"int",  align:"center", width:"100", label:"Годен ДО"},
-        { type:"ro", sort:"int",  align:"center", width:"100", label:"Количество"},
+        { type:"ro", sort:"str",  align:"center", width:"100", label:"Партия" },
+        { type:"ro", sort:"str",  align:"center", width:"100", label:"Годен ДО"},
+        { type:"ro", sort:"str",  align:"center", width:"100", label:"Количество"},
 
     ]
 });
@@ -41,18 +41,16 @@ update = function(){
     });
 };
 
-                    
-self.Toolbars["def"].attachEvent("onClick", function(id) {
-	update();
-});
-      
+
+
+window.change_depart = function(){
+
+    update();
+
+}
+
 update();
 
-
-window.do_print = function(){
-		
-		sw_grid1.printView("","<script> window.print(); </script>");      
-}
 
 
 sw_grid1.attachEvent("onRowSelect", function(code){
@@ -65,3 +63,57 @@ sw_grid1.attachEvent("onRowSelect", function(code){
     self.load(sw_grid2, "/revision/saldo_gap/data/detail?" + self.serialize(params));
 
 });
+
+
+
+window.do_print = function(){
+
+    sw_grid1.printView("","<script> window.print(); </script>");
+}
+
+
+
+window.do_csv = function(){
+
+    self.GridCSV(sw_grid1);
+
+}
+
+
+
+
+window.do_csv_full = function(){
+    /*print_format("csv");*/
+
+
+    params["departs"]  = self.Toolbars["def"].getListOptionSelected("departs");
+
+
+    if (params["departs"] != '3' && params["departs"] != '31'){
+        return false;
+    }
+
+
+
+
+    var new_grid = new dhtmlXGridObject({
+        /*parent: "sw_grid1",*/
+        columns: [
+            { type:"ro", sort:"str",  align:"center", width:"80",  label:"Код" },
+            { type:"ro", sort:"str",  align:"center", width:"100", label:"Наименование" },
+            { type:"ro", sort:"str",  align:"center", width:"100", label:"Товарный остаток"},
+            { type:"ro", sort:"str",  align:"center", width:"100", label:"Адрес"},
+            { type:"ro", sort:"str",  align:"center", width:"100", label:"Остаток в ячейке"},
+            { type:"ro", sort:"str",  align:"center", width:"100", label:"Всего на адресах"}
+
+        ]
+    });
+
+
+    self.load(new_grid, "/revision/saldo_gap/data/csv_full?" + self.serialize(params), function() {
+
+        self.GridCSV(new_grid);
+    });
+
+
+}
--- revision/saldo_gap.py
+++ revision/saldo_gap.py
@@ -20,20 +20,22 @@ class Saldo_GapHandler(BaseHandler):
     urls = r'/revision/auditor'
 
     def get(self):
-                                 
 
-       
-                        
         loader = template.Loader(os.path.join(ROOT_DIR, 'revision'))
         result = loader.load("saldo_gap.js").generate()
        
         all_departs = RC.get_current(rc=self.session.rc).depart_cls
         # кнопки с департаментами РЦ
         
-        departs = [{"id":"%s" % i.id, "type":"button", "text":i.name} for i in all_departs]       
+        departs = [{"id":"%s" % i.id, "type":"button", "text":i.name, "action":"change_depart"} for i in all_departs]
        
         self.write({'def':[
-                           {'type':"button", 'text':'Печать', 'img':"print.gif", 'imgdis':"print_dis.gif", 'action':'do_print'},
+                           #{'type':"button", 'text':'Печать', 'img':"print.gif", 'imgdis':"print_dis.gif", 'action':'do_print'},
+                           #{'type':"separator"},
+
+                           {'type':"button", 'text':'Excel (кратко)',    'action':'do_csv', 'img':"excel24.png", 'imgdis':"excel24g.png"},
+                           {'type':"button", 'text':'Excel (подробно)',  'action':'do_csv_full', 'img':"excel24.png", 'imgdis':"excel24g.png"},
+                           {'type':"separator"},
                            {'type':"text", 'text':"Подразделение:"},
                            {'id':'departs', 'type':"buttonSelect",  'mode':'select', 'selected': all_departs[0].id,  'items':departs},
 
@@ -73,6 +75,21 @@ class Saldo_GapDataHandler(BaseHandler):
             
             self.write(all_results)  
             return
+
+
+
+        if param == 'csv_full':
+
+
+            out = self.cursor.var(cx_Oracle.CURSOR)
+
+            res = self.proc("shiva.dif_saldo_tehno_shiva", [departs, out])
+
+            all_results = fetchall(res[-1])
+
+            self.write(all_results)
+            return
+
 
 
 
